93d8aeeb325538b272c8e270d3e740c3991d9e66
Jason Pitts <juniorjr083@gmail.com>
2018-05-22 23:36:27 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -50,20 +50,16 @@ def valid_move?(board, pos)
 end 
 
 def turn(board) 
-  puts "Please chose a number 1-9."
-  inni = gets.strip
-  index = input_to_index(inni)
-  
-  if index < 0 
-    puts "Please chose a number 1-9."
-    inni = gets.strip
-    
-  end 
-  
-   if valid_move?(board,index)
-     
-    display_board(board)
-  end 
+ puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    char = current_player(board)
+    if valid_move?(board, index)
+      move(board, index, char)
+      display_board(board)
+    else
+      turn(board)
+    end
   
   
 end  
