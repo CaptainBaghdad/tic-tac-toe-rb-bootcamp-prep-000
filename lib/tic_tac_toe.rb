faa50276c692da80a80f5bd384a1bea098b9201c
Jason Pitts <juniorjr083@gmail.com>
2018-05-22 23:37:16 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -74,7 +74,15 @@ def current_player(board)
 end
 
 
-
+def turn_count(board)
+  number_of_turns = 0
+  board.each do |space|
+    if space == "X" || space == "O"
+      number_of_turns += 1
+    end
+  end
+  return number_of_turns
+end
 
 
 
